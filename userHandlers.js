6c50286bf02522735ed0ca9a19e0af3ddba9e412
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2022-11-13 15:32:03 +0100

    challenge express 03

--- userHandlers.js
+++ userHandlers.js
@@ -30,7 +30,25 @@ const getUserById = (req, res) => {
     });
 };
 
+const postUsers = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  databaseUsers
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the users");
+    });
+};
+
 module.exports = {
   getUsers,
+  postUsers,
   getUserById,
 };
