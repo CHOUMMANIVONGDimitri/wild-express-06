a4a077448e0ee0f70b90ed6061d0de98fa375d09
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2022-11-13 16:20:06 +0100

    challenge express 04

--- movieHandlers.js
+++ movieHandlers.js
@@ -12,22 +12,6 @@ const getMovies = (req, res) => {
     });
 };
 
-const postMovies = (req, res) => {
-  const { title, director, year, color, duration } = req.body;
-  database
-    .query(
-      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
-      [title, director, year, color, duration]
-    )
-    .then(([result]) => {
-      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error saving the movie");
-    });
-};
-
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
@@ -46,8 +30,47 @@ const getMovieById = (req, res) => {
     });
 };
 
+const postMovies = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
+
+const updateMovies = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      `UPDATE movies SET title = ?, director = ?, year = ?, color = ?, duration = ? WHERE id = ?`,
+      [title, director, year, color, duration, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the movie");
+    });
+};
+
 module.exports = {
   getMovies,
   postMovies,
   getMovieById,
+  updateMovies,
 };
